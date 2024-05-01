f5fc67e9e04b73656eaaa29ad0940670556a9566
Lam <lam@MacBook-Pro-4.local>
2024-05-01 13:11:53 +0300

    .

---
+++ tools/actor_model/a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <tuple>
+#define f0(i, n) for (int i = 0; i < (n); i++)
+#define f1(i, n) for (int i = 1; i <= (n); i++)
+#define f2(i, n) for (int i = 2; i <= (n); i++)
+#define mp make_pair
+
+using namespace std;
+typedef long long ll;
+const int mod = 1000000007;
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cout << "hello world";
+}
